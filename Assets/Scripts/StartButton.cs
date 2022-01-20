0c4cd140afd9334cf0dcb413973ab8ffd2c5d9e9
0803uni <88020141+0803uni@users.noreply.github.com>
2022-01-20 18:50:05 +0900

    タイトル画面作成
    タイトルシーンを作成
    ゲームスタートボタンを作成
    ゲーム終了ボタンを作成

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Button))]
+public class StartButton : MonoBehaviour
+{
+
+    void Start()
+    {
+        var button = GetComponent<Button>();
+        //ボタンを押したときのリスナー設定
+        button.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("StageScene01");
+        });
+    }
+}
