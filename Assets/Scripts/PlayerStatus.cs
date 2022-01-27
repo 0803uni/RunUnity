17069af5fa844605736759cc3563936320235d9f
0803uni <88020141+0803uni@users.noreply.github.com>
2022-01-27 18:29:50 +0900

    石に当たると、Titleシーンに戻るように設定した

--- Assets/Scripts/PlayerStatus.cs
+++ Assets/Scripts/PlayerStatus.cs
@@ -1,18 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
 public class PlayerStatus : MobStatus
 {
-    // Start is called before the first frame update
-    void Start()
+    protected override void OnDie()
     {
-        
-    }
+        base.OnDie();
 
-    // Update is called once per frame
-    void Update()
+        StartCoroutine(GoToGameOverCoroutine());
+    }
+    private IEnumerator GoToGameOverCoroutine()
     {
-        
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("TitleScene");
     }
 }
