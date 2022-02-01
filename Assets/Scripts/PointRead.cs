c9f53a6c0c41a821480eb890570417ab53dffd2f
0803uni <88020141+0803uni@users.noreply.github.com>
2022-02-01 20:30:20 +0900

    2/1
    PointRead
    ScoreReader
    ToResultsScene
    DetaKeep
    上記のスクリプトを追加

---
+++ Assets/Scripts/PointRead.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PointRead : MonoBehaviour
+{
+    public GameObject score_object = null; // Textオブジェクト
+    public int score_num = 0; // スコア変数
+
+    [SerializeField]
+    DataKeep PointData;
+
+    void Start()
+    {
+        //Debug.Log(data.score);
+        score_object = GameObject.Find("Text");
+    }
+    void Update()
+    {
+        /* オブジェクトからTextコンポーネントを取得
+        Text score_text = score_object.GetComponent<Text>();
+        // テキストの表示を入れ替える
+        score_text.text = "Score:" + score_num.ToString();
+
+        score_num += 1; // とりあえず1加算し続けてみる*/
+    }
+}
+
