93bd60a8d9f6b0d42108893808b05aaec9ed296d
0803uni <88020141+0803uni@users.noreply.github.com>
2022-02-02 20:39:46 +0900

    エラーの所をコメントアウトした

--- Assets/Scripts/PointDeta/DataRead.cs
+++ Assets/Scripts/PointDeta/DataRead.cs
@@ -14,31 +14,31 @@ public class DataRead : MonoBehaviour
 
     
 
-    void Start(DataKeep data)
-    {
-        scoreText.text = "data.score:" + String.Format(format: $"{Math.Abs(data.score * 100)}");
-        Debug.Log(data.score);
-    }
-    private void Awake()
-    {
+   // void Start(DataKeep data)
+    //{
+       // scoreText.text = "data.score:" + String.Format(format: $"{Math.Abs(data.score * 100)}");
+       // Debug.Log(data.score);
+    //}
+   // private void Awake()
+    //{
         //  var a = GameObject.Find("ScoreText");
         //   scoreTextObject = GameObject.Find("ScoreText");
        // scoreText = GetComponent<Text>(data.score);
     }
-    IEnumerator Wait3Seconds()
-    {
-        data.score = GetComponent<Text>(data.score);
+    //IEnumerator Wait3Seconds()
+    //{
+    //    data.score = GetComponent<Text>(data.score);
 
-        yield return new WaitForSeconds(3f);
-        SceneManager.LoadScene("ResultScene");
-    }
+    //    yield return new WaitForSeconds(3f);
+    //    SceneManager.LoadScene("ResultScene");
+    //}
 
-    private int GetComponent<T>(int score)
-    {
-        throw new NotImplementedException();
-    }
+    //private int GetComponent<T>(int score)
+    //{
+    //    throw new NotImplementedException();
+    //}
     //private T GetComponent<T>(int score)
     //{
     //    throw new NotImplementedException();
     //}
-}
+
