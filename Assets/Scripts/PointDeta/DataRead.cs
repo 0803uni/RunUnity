ca6919476291bfb190b5a0d3ec1e9275e0ddbaf0
0803uni <88020141+0803uni@users.noreply.github.com>
2022-02-02 18:17:22 +0900

    2/2

--- Assets/Scripts/PointDeta/DataRead.cs
+++ Assets/Scripts/PointDeta/DataRead.cs
@@ -1,12 +1,44 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class DataRead : MonoBehaviour
 {
     [SerializeField]
     DataKeep data;
+    [SerializeField]
+    GameObject scoreTextObject;
+    Text scoreText;
 
-    void Start()
+    
+
+    void Start(DataKeep data)
     {
+        scoreText.text = "data.score:" + String.Format(format: $"{Math.Abs(data.score * 100)}");
         Debug.Log(data.score);
     }
+    private void Awake()
+    {
+        //  var a = GameObject.Find("ScoreText");
+        //   scoreTextObject = GameObject.Find("ScoreText");
+       // scoreText = GetComponent<Text>(data.score);
+    }
+    IEnumerator Wait3Seconds()
+    {
+        data.score = GetComponent<Text>(data.score);
+
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("ResultScene");
+    }
+
+    private int GetComponent<T>(int score)
+    {
+        throw new NotImplementedException();
+    }
+    //private T GetComponent<T>(int score)
+    //{
+    //    throw new NotImplementedException();
+    //}
 }
--- Assets/Scripts/PointDeta/DataWrite.cs
+++ Assets/Scripts/PointDeta/DataWrite.cs
@@ -5,9 +5,20 @@ public class DataWrite : MonoBehaviour
 {
     [SerializeField]
     DataKeep data;
+    private float totalTime;
+    private object timerTexts;
+    private int retime;
 
     void Update()
     {
+        totalTime -= Time.deltaTime;
+        retime = (int)totalTime;
+        timerTexts = retime.ToString();
+
+        if (retime == 0)
+        {
+            SceneManager.LoadScene("ResultScene");
+        }
         if (Input.GetKeyDown(KeyCode.Space))
         {
             data.score = 100;
