792187295aed6c3addb5ae7e17ff5efc708ab0ec
0803uni <88020141+0803uni@users.noreply.github.com>
2022-02-07 16:56:24 +0900

    タイマーをステージ０１に追加

---
+++ Assets/Scripts/TargetGene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TargetGene : MonoBehaviour
+{
+    private Vector3 targetpos;
+    public Text Scretext;
+    public static int score = 0;
+        void OnCollisonEnter(Collision collison)
+    {
+        score += 10;
+        Scretext.text = string.Format("Socre{0}", score);
+
+        GetComponent<ParticleSystem>().Play();
+    }
+
+    public static int getscore()
+    {
+        return score;
+    }
+    void Start()
+    {
+        targetpos = transform.position;
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerTexts;
+    [SerializeField]
+    float totalTime = 150;
+    int ritime;
+   
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        totalTime -= Time.deltaTime;
+        ritime = (int)totalTime;
+        timerTexts.text = ritime.ToString();
+        if(ritime==0)
+        {
+            SceneManager.LoadScene("ResultScene");
+        }
+    }
+}
